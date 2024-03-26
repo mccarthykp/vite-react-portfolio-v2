eff1845edce4996d044c73dca717c72cafa4741c
Kevin McCarthy <mccarthykp@users.noreply.github.com>
2024-03-26 13:40:26 -0400

    Adjusted Footer component widths

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <footer className="w-1/1 lg:w-3/4 py-8 px:10 lg:mx-auto text-center">
+      <footer className="w-1/1 py-8 px:10 lg:mx-auto text-center">
         <div className="text-xs text-gray-500 md:text-left">
           <p>Built with React. Hosted on GitHub Pages.</p>
           <p>

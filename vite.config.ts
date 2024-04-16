6c0bbadc880c8fc2b0e6d10b1448b2821177e2aa
Kevin McCarthy <mccarthykp@users.noreply.github.com>
2024-04-16 17:39:17 -0400

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'vite-react-portfolio-v2',
   server: {
     watch: {
       usePolling: true

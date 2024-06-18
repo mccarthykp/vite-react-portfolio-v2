a7969588a08bec6c836297f956cfd7de9cd9b2ed
Kevin McCarthy <mccarthykp@users.noreply.github.com>
2024-06-18 11:04:08 -0400

    organized imports

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 // Components
 import Portfolio from "../components/portfolio/Portfolio";
 import BackButton from "../components/buttons/BackButton";
--- src/pages/WalletOverviewPage.tsx
+++ src/pages/WalletOverviewPage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 // Components

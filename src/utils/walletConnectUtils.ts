e83fadc2d5b7e37ad5acff2b27256834e606f70c
Kevin McCarthy <mccarthykp@users.noreply.github.com>
2024-06-20 18:52:09 -0400

    detect mobile

--- src/utils/walletConnectUtils.ts
+++ src/utils/walletConnectUtils.ts
@@ -20,7 +20,7 @@ export const useWalletConnection = () => {
     try {
       if (!provider) {
         if (isMobile()) {
-          window.location.href = 'https://metamask.app.link/dapp/https://www.kevinmccarthy.dev';
+          window.location.href = 'https://metamask.app.link/dapp/www.kevinmccarthy.dev';
           return;
         } else {
           alert("Please install MetaMask extension to connect your wallet.");
@@ -69,7 +69,7 @@ export const useWalletConnection = () => {
       sessionStorage.setItem(SESSION_KEY, JSON.stringify({ isConnected: true, userAddress: account }));
 
       // Redirect back to your website after signing
-      window.location.href = 'https://kevinmccarthy.dev';
+      window.location.href = 'kevinmccarthy.dev';
     } catch (error) {
       console.error("Failed to sign message:", error);
       alert("Failed to sign message.");

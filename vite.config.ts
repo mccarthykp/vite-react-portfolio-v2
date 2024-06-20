85f1d1954fc0d32731c7298b13360ef280c3882b
Kevin McCarthy <mccarthykp@users.noreply.github.com>
2024-06-20 17:35:33 -0400

    config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vite-react-portfolio-v2/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     watch: {
       usePolling: true

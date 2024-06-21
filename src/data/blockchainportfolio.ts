915636888705774eeb49b37c77249459686ab418
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2024-06-21 11:54:48 -0400

    updated projects data, home page margin

--- src/data/blockchainportfolio.ts
+++ src/data/blockchainportfolio.ts
@@ -1,66 +1,64 @@
 export interface BlockchainPortfolioItemData {
-  title: string;
+  description: string;
+  id: number;
+  imgAlt: string;
+  imgUrl: string;
+  link: string;
   subtitle: string;
   tags: string[];
-  imgUrl: string;
-  imgAlt: string;
-  link: string;
-  description: string;
-  id: number; // Optional unique identifier
+  title: string;
 }
 
 const blockchainportfolio: BlockchainPortfolioItemData[] = [
   {
-    title: 'Blockwell Health',
-    subtitle: 'Decentralized Health Management Platform',
-    tags: ['AWS', 'IPFS', 'Solidity', 'HyperLedger Fabric'],
-    imgUrl: '/assets/imgs/blockwell-health-logo.png',
-    imgAlt: 'Blockwell Health Logo',
-    link: 'https://www.linkedin.com/company/blockwell-health',
     description: "Blockwell was founded to help ensure trust and transparency while streamlining healthcare operations in the U.S. Taking a holistic approach to data management, we seamlessly integrate secure and encrypted solutions for electronic prescriptions and decentralized health records.",
     id: 1,
+    imgAlt: 'Blockwell Health Logo',
+    imgUrl: '/assets/imgs/blockwell-health-logo.png',
+    link: 'https://www.linkedin.com/company/blockwell-health',
+    subtitle: 'Decentralized Health Management Platform',
+    tags: ['HyperLedger Fabric', 'IPFS', 'Solidity'],
+    title: 'Blockwell Health',
   },
   {
-    title: 'DEHR',
-    subtitle: 'Electronic Health Records Service',
-    tags: ['IPFS', 'Solidity'],
-    imgUrl: '/assets/imgs/dehr-logo.png',
-    imgAlt: 'DEHR Logo',
-    // link: 'https://www.github.com/mccarthykp/DEHR',
-    link: 'https://www.linkedin.com/company/blockwell-health',
     description: "DEHR provides secure, encrypted storage, sharing, and access to patient health information. This enables healthcare providers to deliver personalized, interoperable care while streamlining administrative processes through robust access control, auditability, and decentralized storage solutions.",
     id: 2,
+    imgAlt: 'DEHR Logo',
+    imgUrl: '/assets/imgs/dehr-logo.png',
+    link: 'https://www.linkedin.com/company/blockwell-health',
+    subtitle: 'Electronic Health Records Service',
+    tags: ['IPFS', 'Solidity'],
+    title: 'DEHR',
   },
   {
-    title: 'FillRx',
-    subtitle: 'E-script Service',
-    tags: ['Solidity'],
-    imgUrl: '/assets/imgs/fillrx-logo.png',
-    imgAlt: 'FillRX Logo',
-    // link: 'https://www.github.com/mccarthykp/FillRx',
-    link: 'https://www.linkedin.com/company/blockwell-health',
     description: "FillRx is a comprehensive web and mobile application designed to seamlessly integrate patients, healthcare providers, and pharmacies, optimizing the prescription and fulfillment journey. Utilizing private blockchain technology, FillRx provides a secure, encrypted, and decentralized solution for the storage and transfer of electronic prescriptions.",
     id: 3,
+    imgAlt: 'FillRX Logo',
+    imgUrl: '/assets/imgs/fillrx-logo.png',
+    link: 'https://www.linkedin.com/company/blockwell-health',
+    subtitle: 'E-script Service',
+    tags: ['Solidity'],
+    title: 'FillRx',
   },
   {
-    title: 'TokenTracker',
-    subtitle: 'Currency Monitoring',
-    tags: ['Vite', 'React', 'TypeScript', 'CoinGecko API'],
-    imgUrl: '/assets/imgs/tokentracker-logo.png',
-    imgAlt: 'TokenTracker Logo',
-    link: 'https://www.github.com/mccarthykp/TokenTracker',
     description: "TokenTracker is a web application that tracks the top 25 cryptocurrencies by market capitalization. It provides real-time updates and allows users to filter coins by price and 24-hour change.",
     id: 4,
+    imgAlt: 'TokenTracker Logo',
+    imgUrl: '/assets/imgs/tokentracker-logo.png',
+    link: 'https://www.github.com/mccarthykp/TokenTracker',
+    subtitle: 'Currency Monitoring',
+    tags: ['CoinGecko API', 'React', 'TypeScript'],
+    title: 'TokenTracker',
   },
-  {
-    title: 'SupplySync',
-    subtitle: 'Smart Contract',
-    tags: ['Truffle', 'Ganache', 'Solidity'],
-    imgUrl: '/assets/imgs/supply-sync-logo.png',
-    imgAlt: 'SupplySync Logo',
-    link: 'https://www.github.com/mccarthykp/SupplySync',
+  { 
     description: "SupplySync is designed to streamline supply chain management through smart contracts. This showcases the potential of decentralized technology and demonstrates basic functionalities required to create and manage supply chain processes.",
     id: 5,
+    imgAlt: 'SupplySync Logo',
+    imgUrl: '/assets/imgs/supply-sync-logo.png',
+    link: 'https://www.github.com/mccarthykp/SupplySync',
+    subtitle: 'Smart Contract',
+    tags: ['Ganache', 'Solidity', 'Truffle'],
+    title: 'SupplySync',
   },
 ];
 
--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -1,34 +1,34 @@
 export interface PortfolioItemData {
-  title: string;
+  description: string;
+  id: number;
+  imgAlt: string;
+  imgUrl: string;
+  link: string;
   subtitle: string;
   tags: string[];
-  imgUrl: string;
-  imgAlt: string;
-  link: string;
-  description: string;
-  id: number; // Optional unique identifier
+  title: string;
 }
 
 const portfolio: PortfolioItemData[] = [
   {
-    title: 'Blockwell Health',
-    subtitle: 'Decentralized EHR Platform',
-    tags: ['HyperLedger Fabric', 'IPFS'],
-    imgUrl: '/assets/imgs/blockwell-health-logo.png',
-    imgAlt: 'Blockwell Health Logo',
-    link: 'https://www.linkedin.com/company/blockwell-health',
     description: "Blockwell was founded to help ensure trust and transparency while streamlining healthcare operations in the U.S. Taking a holistic approach to data management, we seamlessly integrate secure and encrypted solutions for electronic prescriptions and decentralized health records.",
     id: 1,
+    imgAlt: 'Blockwell Health Logo',
+    imgUrl: '/assets/imgs/blockwell-health-logo.png',
+    link: 'https://www.linkedin.com/company/blockwell-health',
+    subtitle: 'Decentralized EHR Platform',
+    tags: ['HyperLedger Fabric', 'IPFS'],
+    title: 'Blockwell Health',
   },
   {
-    title: 'Breach Protocol',
-    subtitle: 'Hacking Minigame',
-    tags: ['React', 'TypeScript', 'Vite'],
-    imgUrl: '/assets/imgs/breach-protocol-logo.png',
-    imgAlt: 'Breach Protocol Logo',
-    link: 'https://github.com/mccarthykp/BreachProtocol',
     description: "Inspired by Cyberpunk 2077, this is an implementation of the Breach Protocol Quickhack minigame. This was built using React with Vite, TypeScript and Tailwind CSS.",
     id: 2,
+    imgAlt: 'Breach Protocol Logo',
+    imgUrl: '/assets/imgs/breach-protocol-logo.png',
+    link: 'https://github.com/mccarthykp/BreachProtocol',
+    subtitle: 'Hacking Minigame',
+    tags: ['React', 'TypeScript'],
+    title: 'Breach Protocol',
   },
   // {
   //   title: 'Poppy & Ash',
@@ -41,56 +41,54 @@ const portfolio: PortfolioItemData[] = [
   //   id: 3,
   // },
   {
-    title: 'SupplySync',
-    subtitle: 'Smart Contract',
-    tags: ['Ganache', 'Solidity', 'Truffle'],
-    imgUrl: '/assets/imgs/supply-sync-logo.png',
-    imgAlt: 'SupplySync Logo',
-    link: 'https://www.github.com/mccarthykp/SupplySync',
     description: "SupplySync is designed to streamline supply chain management through smart contracts. This showcases the potential of decentralized technology and demonstrates basic functionalities required to create and manage supply chain processes.",
     id: 3,
+    imgAlt: 'SupplySync Logo',
+    imgUrl: '/assets/imgs/supply-sync-logo.png',
+    link: 'https://www.github.com/mccarthykp/SupplySync',
+    subtitle: 'Smart Contract',
+    tags: ['Ganache', 'Solidity', 'Truffle'],
+    title: 'SupplySync',
   },
   {
-    title: 'FillRx',
-    subtitle: 'E-script Service',
-    tags: ['Solidity'],
-    imgUrl: '/assets/imgs/fillrx-logo.png',
-    imgAlt: 'FillRX Logo',
-    // link: 'https://www.github.com/mccarthykp/FillRx',
-    link: 'https://www.linkedin.com/company/blockwell-health',
     description: "FillRx is a comprehensive web and mobile application designed to seamlessly integrate patients, healthcare providers, and pharmacies, optimizing the prescription and fulfillment journey. Utilizing private blockchain technology, FillRx provides a secure, encrypted, and decentralized solution for the storage and transfer of electronic prescriptions.",
     id: 4,
+    imgAlt: 'FillRX Logo',
+    imgUrl: '/assets/imgs/fillrx-logo.png',
+    link: 'https://www.linkedin.com/company/blockwell-health',
+    subtitle: 'E-script Service',
+    tags: ['Solidity'],
+    title: 'FillRx',
   },
   {
-    title: 'Tomato Trove',
-    subtitle: 'Web Scraper',
-    tags: ['BeautifulSoup4', 'Python'],
-    imgUrl: '/assets/imgs/tomato-trove-logo.png',
-    imgAlt: 'Tomato Trove Logo',
-    link: 'https://www.github.com/mccarthykp/TomatoTrove',
     description: "A Rotten Tomatoes web scraper that functions with custom queried endpoints to serve scraped data. This was built in Python with BeautifulSoup4.",
     id: 5,
+    imgAlt: 'Tomato Trove Logo',
+    imgUrl: '/assets/imgs/tomato-trove-logo.png',
+    link: 'https://www.github.com/mccarthykp/TomatoTrove',
+    subtitle: 'Web Scraper',
+    tags: ['BeautifulSoup4', 'Python'],
+    title: 'Tomato Trove',
   },
   {
-    title: 'DEHR',
-    subtitle: 'Electronic Health Records Service',
-    tags: ['IPFS', 'Solidity'],
-    imgUrl: '/assets/imgs/dehr-logo.png',
-    imgAlt: 'DEHR Logo',
-    // link: 'https://www.github.com/mccarthykp/DEHR',
-    link: 'https://www.linkedin.com/company/blockwell-health',
     description: "DEHR provides secure, encrypted storage, sharing, and access to patient health information. This enables healthcare providers to deliver personalized, interoperable care while streamlining administrative processes through robust access control, auditability, and decentralized storage solutions.",
     id: 6,
+    imgAlt: 'DEHR Logo',
+    imgUrl: '/assets/imgs/dehr-logo.png',
+    link: 'https://www.linkedin.com/company/blockwell-health',
+    subtitle: 'Electronic Health Records Service',
+    tags: ['IPFS', 'Solidity'],
+    title: 'DEHR',
   },
   {
-    title: 'TokenTracker',
-    subtitle: 'Currency Monitoring',
-    tags: ['CoinGecko API', 'React', 'TypeScript', 'Vite'],
-    imgUrl: '/assets/imgs/tokentracker-logo.png',
-    imgAlt: 'TokenTracker Logo',
-    link: 'https://www.github.com/mccarthykp/TokenTracker',
     description: "TokenTracker is a web application that tracks the top 25 cryptocurrencies by market capitalization. It provides real-time updates and allows users to filter coins by price and 24-hour change.",
     id: 7,
+    imgAlt: 'TokenTracker Logo',
+    imgUrl: '/assets/imgs/tokentracker-logo.png',
+    link: 'https://www.github.com/mccarthykp/TokenTracker',
+    subtitle: 'Currency Monitoring',
+    tags: ['CoinGecko API', 'React', 'TypeScript'],
+    title: 'TokenTracker',
   },
 ];
 

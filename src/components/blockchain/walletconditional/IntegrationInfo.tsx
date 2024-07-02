0da5c2de0f87c78457ed6f8d2396d5e7556daf7a
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2024-07-02 13:28:18 -0400

    Light Mode Styles

--- src/components/blockchain/walletconditional/IntegrationInfo.tsx
+++ src/components/blockchain/walletconditional/IntegrationInfo.tsx
@@ -15,7 +15,7 @@ const IntegrationInfo: React.FunctionComponent<IntegrationInfoProps> = ({
           I'm working on new integrations, check back soon to see more!
         </p>
       ) : (
-        <p className="flex mt-4 bg-neutral-300 dark:bg-opacity-0 bg-opacity-40 rounded-lg pr-2 dark:text-red-300 items-center font-rajdhani font-normal tracking-wide md:text-md text-sm">
+        <p className="flex mt-4 bg-neutral-300 dark:bg-opacity-0 bg-opacity-40 rounded-lg pr-2 dark:text-red-300 items-center font-rajdhani font-medium dark:font-normal tracking-wide md:text-md text-sm">
           <span className="text-red-900 dark:text-red-200 mr-2 text-xl">&#10060;</span>
           Please connect your wallet to view.
         </p>

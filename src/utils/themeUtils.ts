1d0e1e0b5e1e7d309aad46e22d2144de2aafc8c9
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2024-10-23 17:18:13 -0400

    default to dark mode, update packages

--- src/utils/themeUtils.ts
+++ src/utils/themeUtils.ts
@@ -2,6 +2,6 @@ export const applyTheme = () => {
   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
       document.documentElement.classList.add('dark');
   } else {
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.add('dark');
   }
 };
